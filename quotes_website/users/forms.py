9004658930840616744f506e4a229df500947acc
KossKokos <koskokos0@gmail.com>
2023-11-26 18:29:06 +0000

    hw_13

--- quotes_website/users/forms.py
+++ quotes_website/users/forms.py
@@ -1,22 +1,24 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.forms import CharField, TextInput, PasswordInput
+from django.forms import CharField, TextInput, PasswordInput, EmailField, EmailInput
 
 
 class RegisterForm(UserCreationForm):
     username = CharField(max_length=100, widget=TextInput())
+    email = EmailField(widget=EmailInput())
     password1 = CharField(max_length=20, min_length=8, widget=PasswordInput())
     password2 = CharField(max_length=20, min_length=8, widget=PasswordInput())
 
     class Meta:
         model = User
-        fields = ("username", "password1", "password2")
+        fields = ("username", "email", "password1", "password2")
 
 
 class LoginForm(AuthenticationForm):
-    username = CharField(max_length=100, required=True, widget=TextInput())
+    # username = CharField(max_length=100, required=True, widget=TextInput())
+    email = EmailField(widget=EmailInput())
     password = CharField(max_length=20, min_length=8, required=True, widget=PasswordInput())
 
     class Meta:
         model = User
-        fields = ("username", "password")
+        fields = ("email", "password")
